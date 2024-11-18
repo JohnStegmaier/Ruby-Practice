fb426cdc0ee6a0874ce4c5d5b1c5de4d864a1fac
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-17 21:36:27 -0500

    Add code to retrieve first name from user and print to command line

--- Section 2: The Ruby Programming Language/03 Getting Input From The User.rb
+++ Section 2: The Ruby Programming Language/03 Getting Input From The User.rb
@@ -1,3 +1,7 @@
 # Getting Input From The User
 # By: John Stegmaier
 # Date: November 17th 2024
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Thank you, you said your first name is #{first_name}."
