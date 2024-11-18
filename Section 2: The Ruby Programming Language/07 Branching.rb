e03f96b89cfc91e8d006baccfbf623b4aca5d991
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-18 16:57:37 -0500

    Add else if branching example

--- Section 2: The Ruby Programming Language/07 Branching.rb
+++ Section 2: The Ruby Programming Language/07 Branching.rb
@@ -18,3 +18,14 @@ end
 # when a branch is executed, it will jump past the rest and go straight to the end
 # this below text is outside of the branch and will therefore be executed regardless
 puts "outside the branch statement"
+
+# example of using an else if statement
+name = "John"
+
+if name == "John"
+  puts "Welcome to my demo application #{name}"
+elsif name == "Mashrur"
+  puts "Thank you for checking out my code #{name}"
+else
+  puts "Hello and welcome to the program #{name}"
+end
