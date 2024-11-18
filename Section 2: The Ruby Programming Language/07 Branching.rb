24f8c498160d55a8b3effcdfa1c10ce5934c9598
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-18 16:51:12 -0500

    Add if/else output

--- Section 2: The Ruby Programming Language/07 Branching.rb
+++ Section 2: The Ruby Programming Language/07 Branching.rb
@@ -4,8 +4,14 @@
 
 # if/else
 if true
-  #execute some code
-  # else
+  puts "Hello"
+
+# else
 else
-  #execute some other code
+  puts "Bye"
 end
+
+# if / else blocks are terminated with the end statement
+# when a branch is executed, it will jump past the rest and go straight to the end
+# this below text is outside of the branch and will therefore be executed regardless
+puts "outside the branch statement"
