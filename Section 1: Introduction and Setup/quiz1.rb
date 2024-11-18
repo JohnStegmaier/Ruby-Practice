9938a2a605563467b17a46472fa836ed3f25d1ce
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-17 20:09:58 -0500

    Add print statement for sixth element

--- Section 1: Introduction and Setup/quiz1.rb
+++ Section 1: Introduction and Setup/quiz1.rb
@@ -2,6 +2,9 @@
 # By: John Stegmaier
 # November 17th 2024
 
-#Defining and printing an array
+# Defining and printing an array
 address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 p address
+
+# Print the sixth element of the array
+p address[5]
