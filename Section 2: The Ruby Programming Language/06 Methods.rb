21d5a3163beb229cac7942f9a3afb88b1bd79c44
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-18 15:13:52 -0500

    Move multiply method declaration to fix missing or undeclared value error

--- Section 2: The Ruby Programming Language/06 Methods.rb
+++ Section 2: The Ruby Programming Language/06 Methods.rb
@@ -2,6 +2,17 @@
 # By: John Stegmaier
 # Date: November 18th 2024
 
+#NOTE: methods have to be defined ahead of their use in ruby or else your application will throw a fit
+# at runtime 🥲
+
+# Here is an example of a method. We define it with def and then the method names and then the variables.
+# the return CAN be provided by using return {expression} but in ruby, if you don't use return, the return
+# value is implied based off of the last operation / variable / expression / whatever. So in the below case,
+# first_num * second_num will be the return value
+def multiply (first_num, second_num)
+  first_num.to_f * second_num.to_f
+end
+
 puts "Simple calculator"
 20.times { print "-" }
 
@@ -16,10 +27,3 @@ puts "The first number subtracted by the second number is: #{first_number.to_f -
 puts "The first number added to the second number is: #{first_number.to_f + second_number.to_f}"
 puts "the first number mod the second number is: #{first_number.to_f % second_number.to_f}"
 
-# Here is an example of a method. We define it with def and then the method names and then the variables.
-# the return CAN be provided by using return {expression} but in ruby, if you don't use return, the return
-# value is implied based off of the last operation / variable / expression / whatever. So in the below case,
-# first_num * second_num will be the return value
-def multiply (first_num, second_num)
-  first_num.to_f * second_num.to_f
-end
