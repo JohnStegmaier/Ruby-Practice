5423d401327bb83644fc408553de125772febb26
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-17 21:37:24 -0500

    Add code to multiply a string by two

--- Section 2: The Ruby Programming Language/03 Getting Input From The User.rb
+++ Section 2: The Ruby Programming Language/03 Getting Input From The User.rb
@@ -5,3 +5,7 @@
 puts "What is your first name?"
 first_name = gets.chomp
 puts "Thank you, you said your first name is #{first_name}."
+
+puts "Enter a number to multiply by 2"
+input = gets.chomp
+puts input * 2
