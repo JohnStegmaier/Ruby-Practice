0458aa6af13a733e1398a3ca009dc6b4802e2b98
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-18 22:00:31 -0500

    Add elegant solution

--- Projects/Project 2: Area code dictionary /Dial Book.rb
+++ Projects/Project 2: Area code dictionary /Dial Book.rb
@@ -1,6 +1,7 @@
 # Dial book
 # By: John Stegmaier
 # Date: November 18th 2024
+
 dial_book = {
   "newyork" => "212",
   "newbrunswick" => "732",
@@ -18,10 +19,26 @@ dial_book = {
 
 # Get city anmes from the hash
 def get_city_names(somehash)
-  # Write code here
+  list_of_cities = []
+  somehash.each { |key, value| list_of_cities.push(key)}
+  list_of_cities
 end
 
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-  # Write code here
+  somehash.has_key?(key) ? somehash[key] : "not in the provided dictionary"
+end
+
+# Execution flow
+loop do
+  puts "Do you want to look up an area code based on a city name? (Y/N)"
+  answer = gets.chomp.upcase
+  break if answer != "Y"
+
+  puts "Please select a city from the list:"
+  get_city_names(dial_book).each {|entry| puts entry.capitalize}
+  print "Selection:"
+  city = gets.chomp.downcase
+
+  puts "The area code for #{city} is #{get_area_code(dial_book, city)}."
 end
