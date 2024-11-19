7348fd2c618f5e900d03bb5f701ee18a1358dd00
JohnStegmaier <John.Stegmaier@protonmail.com>
2024-11-18 21:36:01 -0500

    Add Style Guide Example File

---
+++ Section 2: The Ruby Programming Language/10 Styling In Ruby.rb
@@ -0,0 +1,22 @@
+# Styling in Ruby
+# By: John Stegmaier
+# Date: November 18th 2024
+
+# In Ruby and Ruby on Rails you want to use Snake Case for method names and variable assignments
+this_is_snake_case
+
+def my_name_is
+  # Global convention in ruby and ruby on rails is that indentation (one tab) is two spaces
+end
+
+# In general, class names are camel case
+# Class names start capitalized (like Java)
+CamelCase
+
+# True CamelCase looks like this:
+camelCaseAnotherCase # BUT WE DON'T USE THIS NAMING CONVENTION IN RUBY!
+class SomeClass
+
+end
+
+# A guide that goes really in depth: https://github.com/rubocop/ruby-style-guide
