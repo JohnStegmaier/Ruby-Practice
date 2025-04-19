1ca7cbc0a139acb63a54cf94f21428773af6b64e
JohnStegmaier <John.Stegmaier@protonmail.com>
2025-04-19 01:00:35 -0400

    Implement Try/Catch for edge case where balance is somehow provided as a string

--- Projects/Project C1: ATM Machine/ATM.rb
+++ Projects/Project C1: ATM Machine/ATM.rb
@@ -1,7 +1,14 @@
 def display_balance(account_balance)
-  if account_balance < 0
-    "Balance: -$#{account_balance.abs}"
-  elsif account_balance > 0
-    "Balance: $#{account_balance}"
+  # return_value
+  begin
+    if account_balance.negative?
+      return_value = "Balance: -$#{account_balance.abs}"
+    elsif account_balance.positive?
+      return_value = "Balance: $#{account_balance}"
+    end
+  rescue StandardError
+    nil
+  else
+    return_value
   end
 end
--- Projects/Project C1: ATM Machine/test/test_ATM.rb
+++ Projects/Project C1: ATM Machine/test/test_ATM.rb
@@ -9,4 +9,8 @@ class TestATM < Minitest::Test
   def test_display_balance_with_negative_amount
     assert_equal 'Balance: -$200', display_balance(-200)
   end
+
+  def test_display_balance_with_string
+    assert_nil display_balance("aString")
+  end
 end
